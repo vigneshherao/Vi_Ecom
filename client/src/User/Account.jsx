19dcaa6f1f6b51b489ac297ddb2c9a3afd950fac
vignesh <vigneshfornavy@gmail.com>
2024-05-29 23:44:03 +0530

    Admin dashboard UI changes

--- client/src/User/Account.jsx
+++ client/src/User/Account.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { logoutUser } from "../utils/slice/userSlice";
 import { Link } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { FaPlusCircle, FaEdit, FaTrashAlt } from "react-icons/fa";
 
 const Account = () => {
   const dispatch = useDispatch();
@@ -18,7 +19,7 @@ const Account = () => {
     })
       .then((response) => {
         dispatch(logoutUser());
-        console.log("logout sucessfull");
+        console.log("logout successful");
       })
       .catch((error) => {
         console.log(error);
@@ -26,55 +27,42 @@ const Account = () => {
   };
 
   return (
-    <div className="bg-gray-100  w-full h-screen p-10 border">
-            <Toaster
-  position="bottom-left"
-  reverseOrder={true}
-/>
-      <div className="flex justify-between px-5 items-center">
-        <div>
-          <h2 className="text-center text-3xl font-extralight bg-blue-300 p-2">
-            Hello {user}
-          </h2>
-        </div>
-        <div>
-          <h2 className="text-center text-3xl bg-black text-white font-semibold p-2 rounded-md">
-           <Link to={"/"}>Home</Link>
-          </h2>
-        </div>
-        <div className="items-center justify-center">
-          <button className="bg-blue-500 text-white p-2" onClick={handleLogout}>
+    <div className="bg-gray-100 w-full h-screen p-10">
+      <Toaster position="bottom-left" reverseOrder={true} />
+      <div className="flex justify-between items-center bg-white p-5 shadow-md rounded-lg">
+        <h2 className="text-3xl font-light text-blue-600">
+          Hello, {user}
+        </h2>
+        <div className="flex space-x-4 items-center">
+          <Link to="/" className="text-xl bg-black text-white font-semibold py-2 px-4 rounded-md hover:bg-gray-800">
+            Home
+          </Link>
+          <button className="bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600" onClick={handleLogout}>
             Logout
           </button>
         </div>
       </div>
-      <div className="flex justify-evenly flex-wrap p-10 mt-20">
-        <div className="justify-center items-center m-3">
-          <img
-            src="https://www.pngall.com/wp-content/uploads/10/Plus-Symbol-Background-PNG.png"
-            className="h-10 w-10 m-auto"
-          />
-        <Link to={"/add"}>
-        <button className="bg-black p-2 text-white mt-4 font-semibold hover:text-blue-300">
-            Add Product
-          </button>
-        </Link>
+      <div className="flex justify-center items-center mt-10">
+        <h1 className="text-4xl font-semibold text-gray-800">Admin Dashboard</h1>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-10 p-10 mt-10">
+        <div className="bg-white shadow-lg rounded-lg p-6 text-center hover:shadow-xl transition-shadow duration-300">
+          <FaPlusCircle className="text-4xl text-blue-500 m-auto" />
+          <Link to="/add">
+            <button className="bg-black text-white py-2 px-6 mt-4 rounded-md font-semibold hover:bg-gray-800">
+              Add Product
+            </button>
+          </Link>
         </div>
-        <div className="justify-center items-center m-3">
-          <img
-            src="https://www.pngmart.com/files/23/Edit-Icon-PNG-Photos.png"
-            className="h-10 w-10 m-auto"
-          />
-          <button className="bg-black p-2 text-white mt-4 font-semibold hover:text-blue-300">
+        <div className="bg-white shadow-lg rounded-lg p-6 text-center hover:shadow-xl transition-shadow duration-300">
+          <FaEdit className="text-4xl text-yellow-500 m-auto" />
+          <button className="bg-black text-white py-2 px-6 mt-4 rounded-md font-semibold hover:bg-gray-800">
             Edit Product
           </button>
         </div>
-        <div className="justify-center items-center m-3">
-          <img
-            src="https://www.freeiconspng.com/thumbs/delete-button-png/blue-delete-button-png-29.png"
-            className="h-10 w-10 m-auto"
-          />
-          <button className="bg-black p-2 text-white mt-4 font-semibold hover:text-blue-300">
+        <div className="bg-white shadow-lg rounded-lg p-6 text-center hover:shadow-xl transition-shadow duration-300">
+          <FaTrashAlt className="text-4xl text-red-500 m-auto" />
+          <button className="bg-black text-white py-2 px-6 mt-4 rounded-md font-semibold hover:bg-gray-800">
             Delete Product
           </button>
         </div>
